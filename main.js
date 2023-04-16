35888fd656662888829ba486a17df33765128736
Paolo <paoloagva@gmail.com>
2023-04-16 15:56:36 +0200

    Added computed properties

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ const app = Vue.createApp({
         return {
             cart: 0,
             product: "Socks",
+            brand: "Vue Mastery",
             description: "This describes the goddamned socks",
-            image: "./assets/images/socks_green.jpg",
+            selectedVariant: 0,
             url: "https://imgur.com/gallery/6fksvmE",
-            inStock: false,
             onSale: true,
             inventory: 0,
             details: ["50% cotton", "30% wool", "20% polyester"],
@@ -15,11 +15,13 @@ const app = Vue.createApp({
                     id: 2234,
                     color: "green",
                     image: "./assets/images/socks_green.jpg",
+                    quantity: 50,
                 },
                 {
                     id: 2235,
                     color: "blue",
                     image: "./assets/images/socks_blue.jpg",
+                    quantity: 0,
                 },
             ],
             sizes: ["XS", "S", "M", "L", "XL"],
@@ -32,8 +34,24 @@ const app = Vue.createApp({
         removeFromCart() {
             this.cart > 0 ? (this.cart -= 1) : (this.cart = 0);
         },
-        updateImage(variantImage) {
-            this.image = variantImage;
+        updateVariant(index) {
+            this.selectedVariant = index;
+        },
+    },
+    computed: {
+        title() {
+            return this.brand + " " + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].image;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity;
+        },
+        producOnSale() {
+            return this.onSale
+                ? this.brand + this.product + " is on sale!"
+                : "";
         },
     },
 });
