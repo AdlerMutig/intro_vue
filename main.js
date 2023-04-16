b4a460a04580e6330b2efd92820fd04b54c4cb53
Paolo <paoloagva@gmail.com>
2023-04-16 15:12:52 +0200

    Added basic event handling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: "Socks",
             description: "This describes the goddamned socks",
             image: "./assets/images/socks_green.jpg",
@@ -10,10 +11,29 @@ const app = Vue.createApp({
             inventory: 0,
             details: ["50% cotton", "30% wool", "20% polyester"],
             variants: [
-                { id: 2234, color: "green" },
-                { id: 2235, color: "blue" },
+                {
+                    id: 2234,
+                    color: "green",
+                    image: "./assets/images/socks_green.jpg",
+                },
+                {
+                    id: 2235,
+                    color: "blue",
+                    image: "./assets/images/socks_blue.jpg",
+                },
             ],
             sizes: ["XS", "S", "M", "L", "XL"],
         };
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        removeFromCart() {
+            this.cart > 0 ? (this.cart -= 1) : (this.cart = 0);
+        },
+        updateImage(variantImage) {
+            this.image = variantImage;
+        },
     },
 });
