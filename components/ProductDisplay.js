7045b1fd1d6c1a041438a581c5173e2e5a1b9bf5
Paolo <paoloagva@gmail.com>
2023-04-16 17:33:45 +0200

    Added both way binding in form

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -49,6 +49,8 @@ app.component("product-display", {
                 </button>
             </div>
         </div>
+        <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+        <review-form @review-submitted="addReview"></review-form>
     </div>`,
     data() {
         return {
@@ -75,6 +77,7 @@ app.component("product-display", {
                 },
             ],
             sizes: ["XS", "S", "M", "L", "XL"],
+            reviews: [],
         };
     },
     methods: {
@@ -89,6 +92,9 @@ app.component("product-display", {
         },
         updateVariant(index) {
             this.selectedVariant = index;
+        },
+        addReview(review) {
+            this.reviews.push(review);
         },
     },
     computed: {
