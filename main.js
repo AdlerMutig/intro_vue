dfd71c600848d230d3bd70b625dfbf5ec550a0ca
Paolo <paoloagva@gmail.com>
2023-04-16 14:52:37 +0200

    Added display logic for lists

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ const app = Vue.createApp({
             inStock: false,
             onSale: true,
             inventory: 0,
+            details: ["50% cotton", "30% wool", "20% polyester"],
+            variants: [
+                { id: 2234, color: "green" },
+                { id: 2235, color: "blue" },
+            ],
+            sizes: ["XS", "S", "M", "L", "XL"],
         };
     },
 });
