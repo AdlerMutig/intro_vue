f4e36b369e582bb9af6ec77b4b1ddd8c6c670226
Paolo <paoloagva@gmail.com>
2023-04-16 16:09:27 +0200

    Stored in a product display component

--- main.js
+++ main.js
@@ -2,56 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             cart: 0,
-            product: "Socks",
-            brand: "Vue Mastery",
-            description: "This describes the goddamned socks",
-            selectedVariant: 0,
-            url: "https://imgur.com/gallery/6fksvmE",
-            onSale: true,
-            inventory: 0,
-            details: ["50% cotton", "30% wool", "20% polyester"],
-            variants: [
-                {
-                    id: 2234,
-                    color: "green",
-                    image: "./assets/images/socks_green.jpg",
-                    quantity: 50,
-                },
-                {
-                    id: 2235,
-                    color: "blue",
-                    image: "./assets/images/socks_blue.jpg",
-                    quantity: 0,
-                },
-            ],
-            sizes: ["XS", "S", "M", "L", "XL"],
         };
     },
-    methods: {
-        addToCart() {
-            this.cart += 1;
-        },
-        removeFromCart() {
-            this.cart > 0 ? (this.cart -= 1) : (this.cart = 0);
-        },
-        updateVariant(index) {
-            this.selectedVariant = index;
-        },
-    },
-    computed: {
-        title() {
-            return this.brand + " " + this.product;
-        },
-        image() {
-            return this.variants[this.selectedVariant].image;
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity;
-        },
-        producOnSale() {
-            return this.onSale
-                ? this.brand + this.product + " is on sale!"
-                : "";
-        },
-    },
+    methods: {},
 });
