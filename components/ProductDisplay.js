3fc3dcd84ccf0d042099246c3912ccba27d07a72
Paolo <paoloagva@gmail.com>
2023-04-16 16:51:28 +0200

    Added subcomponent for product details

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -21,9 +21,8 @@ app.component("product-display", {
                 <p v-if="inStock">In Stock</p>
                 <p v-else>Out of Stock</p>
                 <p>Shipping: {{shipping}}</p>
-                <ul>
-                    <li v-for="detail in details">{{ detail }}</li>
-                </ul>
+                
+                <product-details :details="details"></product-details>
 
                 <div
                     class="color-circle"
